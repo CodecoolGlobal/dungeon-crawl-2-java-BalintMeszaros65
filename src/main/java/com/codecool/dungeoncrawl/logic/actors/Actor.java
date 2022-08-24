ad1ee1f203d3283914efea05fc8fd16cc3c35c2e
Balint Meszaros <meszaros.balint65@gmail.com>
2022-08-24 16:56:56 +0200

    Add: getCellNeighborItem

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -3,6 +3,7 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.Drawable;
+import com.codecool.dungeoncrawl.logic.items.Item;
 
 // TODO public / private methods
 public abstract class Actor implements Drawable {
