bdc152262e996d3257bba5d928fd9707ec21602a
Tallódi Márk <mark.tallodi@gmail.com>
2022-09-07 08:54:11 +0200

    Refactor: Sounds.java delete toString and add values for enums

--- src/main/java/com/codecool/dungeoncrawl/Sound.java
+++ src/main/java/com/codecool/dungeoncrawl/Sound.java
@@ -5,23 +5,24 @@ import java.io.File;
 import java.io.IOException;
 
 public enum Sound {
-    MAIN_MENU,
-    PLAY_GAME,
-    MOVE,
-    PUNCH,
-    SWORD_DUEL,
-    DAMAGE,
-    DIE,
-    HEAL_UP,
-    OPEN_DOOR,
-    GOING_UP_OR_DOWN_ON_STAIRS,
-    PICK_UP_ITEM,
-    ZOMBIE,
-    GHOST,
-    SKELETON;
+    MAIN_MENU("MainMenu.wav"),
+    PLAY_GAME("PlayGame.wav"),
+    MOVE("Move.wav"),
+    PUNCH("Punch.wav"),
+    SWORD_DUEL("SwordDuel"),
+    DAMAGE("Damage.wav"),
+    DIE("Die.wav"),
+    HEAL_UP("HealUp.wav"),
+    OPEN_DOOR("OpenDoor.wav"),
+    GOING_UP_OR_DOWN_ON_STAIRS("GoingUpDownStairs.wav"),
+    PICK_UP_ITEM("PickUpItem.wav"),
+    ZOMBIE("ZombieSound.wav"),
+    GHOST("GhostSound.wav"),
+    SKELETON("SkeletonSound.wav");
 
 
-
+    Sound(String s) {
+    }
 
     public void playSound(String soundFile) {
         try {
@@ -36,40 +37,5 @@ public enum Sound {
             });
         } catch (UnsupportedAudioFileException | LineUnavailableException | IOException ignored){}
 
-    }
-
-    public String toString(Sound sound){
-        switch (sound){
-            case MAIN_MENU:
-                return "MainMenu.wav";
-            case PLAY_GAME:
-                return "PlayGame.wav";
-            case MOVE:
-                return "Move.wav";
-            case PUNCH:
-                return "Punch.wav";
-            case SWORD_DUEL:
-                return "SwordDuel";
-            case DAMAGE:
-                return "Damage.wav";
-            case DIE:
-                return "Die.wav";
-            case HEAL_UP:
-                return "HealUp.wav";
-            case OPEN_DOOR:
-                return "OpenDoor.wav";
-            case GOING_UP_OR_DOWN_ON_STAIRS:
-                return "GoingUpDownStairs.wav";
-            case PICK_UP_ITEM:
-                return "PickUpItem.wav";
-            case ZOMBIE:
-                return "ZombieSound.wav";
-            case GHOST:
-                return "GhostSound.wav";
-            case SKELETON:
-                return "SkeletonSound.wav";
-            default:
-                return "Sound not found";
-        }
     }
 }
