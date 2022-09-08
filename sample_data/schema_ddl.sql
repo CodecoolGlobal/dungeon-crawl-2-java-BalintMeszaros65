d726a2877cbf3ef23f5d2d7d320051ab70107261
Tallódi Márk <mark.tallodi@gmail.com>
2022-09-08 17:23:06 +0200

    Refactor: delete map table and extend game_state

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -11,17 +11,12 @@ DROP TABLE IF EXISTS public.game_state;
 CREATE TABLE public.game_state (
     id serial NOT NULL PRIMARY KEY,
     saved_title text NOT NULL,
-    current_map text NOT NULL,
+    current_map int NOT NULL,
+    map1 text NOT NULL,
+    map2 text NOT NULL,
+    map3 text NOT NULL,
     saved_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     player_id integer NOT NULL
-);
-
-DROP TABLE IF EXISTS public.map;
-CREATE TABLE public.map (
-    id serial NOT NULL PRIMARY KEY,
-    player_id integer NOT NULL,
-    map_name text NOT NULL,
-    map_state text NOT NULL
 );
 
 ALTER TABLE ONLY public.game_state
