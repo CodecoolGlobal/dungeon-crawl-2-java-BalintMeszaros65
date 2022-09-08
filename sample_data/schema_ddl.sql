a0048aa9fdedd5506295e3201ac1cae19f8a3f43
Tallódi Márk <mark.tallodi@gmail.com>
2022-09-08 16:49:52 +0200

    Refactor: foreign key on delete cascade

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS public.player;
+CREATE TABLE public.player (
+   id serial NOT NULL PRIMARY KEY,
+   player_name text NOT NULL,
+   hp integer NOT NULL,
+   x integer NOT NULL,
+   y integer NOT NULL
+);
+
 DROP TABLE IF EXISTS public.game_state;
 CREATE TABLE public.game_state (
     id serial NOT NULL PRIMARY KEY,
@@ -7,24 +16,15 @@ CREATE TABLE public.game_state (
     player_id integer NOT NULL
 );
 
-DROP TABLE IF EXISTS public.player;
-CREATE TABLE public.player (
-    id serial NOT NULL PRIMARY KEY,
-    player_name text NOT NULL,
-    hp integer NOT NULL,
-    x integer NOT NULL,
-    y integer NOT NULL
-);
-
 DROP TABLE IF EXISTS public.map;
 CREATE TABLE public.map (
     id serial NOT NULL PRIMARY KEY,
-    game_id integer NOT NULL,
+    player_id integer NOT NULL,
     map_name text NOT NULL,
     map_state text NOT NULL
 );
 
 ALTER TABLE ONLY public.game_state
-    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id) ON DELETE CASCADE;
 ALTER TABLE ONLY public.map
-    ADD CONSTRAINT fk_game_id FOREIGN KEY (game_id) REFERENCES public.game_state(id);
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id) ON DELETE CASCADE;
