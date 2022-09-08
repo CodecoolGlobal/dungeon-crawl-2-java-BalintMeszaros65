1df317bafc57ee591c2aaaf8c9de8ecad51d0844
Tallódi Márk <mark.tallodi@gmail.com>
2022-09-08 16:48:47 +0200

    Add: popup menu basics

--- src/main/java/com/codecool/dungeoncrawl/Util.java
+++ src/main/java/com/codecool/dungeoncrawl/Util.java
@@ -9,6 +9,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.TextAlignment;
 
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -18,13 +21,14 @@ public class Util {
     static double canvasHeight;
     static BorderPane borderPane;
     static Random random = new Random();
-    static final List<String> DEVELOPERS = new ArrayList<>(List.of("Ágoston", "Ákos", "Bálint", "Márk"));
+    static final List<String> DEVELOPERS = new ArrayList<>(List.of("Ákos", "Bálint", "Márk"));
     private static String namePopup() {
         TextInputDialog td = new TextInputDialog("Knighty McKnight");
         td.setHeaderText("enter your name");
         td.showAndWait();
         return td.getEditor().getText();
     }
+
 
     public static String setUpPlayerName(GameMap map) {
         String playerName = namePopup();
@@ -48,5 +52,24 @@ public class Util {
 
     public static int randInt(int min, int max) {
         return random.nextInt((max - min) + 1) + min;
+    }
+
+    public static void PopupMenu(){
+        final JFrame f= new JFrame("Main Menu");
+        final JPopupMenu mainMenu = new JPopupMenu("MainMenu");
+        JMenuItem playMenu = new JMenuItem("Play");
+        JMenuItem loadMenu = new JMenuItem("Load");
+        //playMenu.setAction();
+        mainMenu.add(playMenu);
+        mainMenu.add(loadMenu);
+        f.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                mainMenu.show(f , e.getX(), e.getY());
+            }
+        });
+        f.add(mainMenu);
+        f.setSize(300,300);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
